6e847a98dc190e05c13da84286492e6fc3626cef
Laurens <laurens.ista@teamrockstars.nl>
2024-09-16 00:33:06 +0200

    Add history animation on new run

--- components/RunHistory.vue
+++ components/RunHistory.vue
@@ -3,12 +3,11 @@
     <template #header>
       <h1 class="text-xl">{{ $t('ui.logs.title')}}</h1>
     </template>
-
-    <ul class="max-h-72 overflow-y-auto">
-      <li v-for="run of pastRuns.toReversed()" :key="run.dungeonId">
-        <DungeonResultItem :run="run" />
-      </li>
-    </ul>
+      <TransitionGroup name="list" tag="ul" class="max-h-72 overflow-y-auto">
+        <li v-for="run of pastRuns.toReversed()" :key="run.dateTime">
+            <DungeonResultItem :run="run" />
+        </li>
+      </TransitionGroup>
   </UCard>
 </template>
 
@@ -18,3 +17,15 @@ import type { RunDungeonResult } from "~/server/api/dungeon/run.post"
 const props = defineProps<{ pastRuns: Array<RunDungeonResult> }>()
 const { pastRuns } = toRefs(props)
 </script>
+
+<style>
+.list-enter-active,
+.list-leave-active {
+  transition: all 1s ease-in;
+}
+.list-enter-from,
+.list-leave-to {
+  opacity: 0;
+  transform: translateY(-20px);
+}
+</style>
