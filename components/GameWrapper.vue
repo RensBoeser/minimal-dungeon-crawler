a0ebd3706d7e87f0f74bcdd7b1eb5492c4bb0ba4
Rens Boeser <r.boeser@sencon.nl>
2024-09-18 23:29:54 +0200

    Fix responsiveness for mobile

--- components/GameWrapper.vue
+++ components/GameWrapper.vue
@@ -1,12 +1,12 @@
 <template>
-  <div class="flex flex-wrap gap-4">
-    <UserInventory v-model:experience="experience" v-model:weapon="weapon" v-model:gold="gold" v-model:inventory="inventory" class="min-w-96" />
+  <div class="flex flex-wrap gap-4 mb-12">
+    <UserInventory v-model:experience="experience" v-model:weapon="weapon" v-model:gold="gold" v-model:inventory="inventory" class="w-96" />
 
-    <DungeonRunner v-model:experience="experience" v-model:inventory="inventory" v-model:gold="gold" :weapon-id="weapon" class="min-w-96" />
+    <DungeonRunner v-model:experience="experience" v-model:inventory="inventory" v-model:gold="gold" :weapon-id="weapon" class="w-96" />
 
-    <div class="flex flex-col gap-4">
-      <DungeonViewer class="min-w-96" />
-      <WeaponShop v-model:weapon="weapon" v-model:gold="gold" class="min-w-96 flex-1" />
+    <div class="flex flex-col gap-4 w-96">
+      <DungeonViewer />
+      <WeaponShop v-model:weapon="weapon" v-model:gold="gold" class="flex-1" />
     </div>
   </div>
 </template>
