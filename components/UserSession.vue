f873deb4823fc88f50a27106ac355ad6ca6dfd5a
Rens Boeser <r.boeser@sencon.nl>
2024-09-20 17:39:44 +0200

    Update github login

--- components/UserSession.vue
+++ components/UserSession.vue
@@ -4,12 +4,12 @@ const { loggedIn, user, session, clear } = useUserSession()
 
 <template>
   <div v-if="loggedIn">
-    <h1>Welcome {{ user.login }}!</h1>
-    <p>Logged in since {{ session.loggedInAt }}</p>
-    <button @click="clear">Logout</button>
+    {{ user }}
+    {{ session }}
+    <UButton @click="clear">Logout</UButton>
   </div>
   <div v-else>
     <h1>Not logged in</h1>
-    <UButton to="/auth/github">Login with GitHub</UButton>
+    <UButton to="/api/auth/github" icon="i-simple-icons-github" label="Login with GitHub" color="black" external />
   </div>
 </template>
