b6fc5ca3671129062387a9dad0b3548dae5b1ac7
Rens Boeser <r.boeser@sencon.nl>
2024-09-25 11:02:34 +0200

    Set logs to only dev

--- server/middleware/log.ts
+++ server/middleware/log.ts
@@ -1,3 +1,7 @@
 export default defineEventHandler(async (event) => {
+  if (process.env.NODE_ENV !== "development") {
+    return
+  }
+
   console.log(event.context.userId, event.method, event.path)
 })
